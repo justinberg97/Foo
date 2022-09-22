c623f4f93a15e5bd58b398489cb93fdcebb7cd30
andersonmourot <109611768+andersonmourot@users.noreply.github.com>
2022-09-22 18:26:22 -0500

    Added API links and JQuery link
    Added API links and JQuery link

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const settingsScores = {
+var settingsScores = {
     "async": true,
 	"crossDomain": true,
 	"url": "https://sportspage-feeds.p.rapidapi.com/teams?league=NFL",
@@ -14,9 +14,7 @@ $.ajax(settingsScores).done(function (response) {
 });
 
 
-
-
-const settingsWeather = {
+var settingsWeather = {
 	"async": true,
 	"crossDomain": true,
 	"url": "https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location=Washington%2CDC%2CUSA&contentType=csv&unitGroup=us&shortColumnNames=0",
@@ -30,3 +28,21 @@ const settingsWeather = {
 $.ajax(settingsWeather).done(function (response) {
 	console.log(response);
 });
+
+
+var settingsGames = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://sportspage-feeds.p.rapidapi.com/games",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "7446481de7mshc58650d6922c359p14ec79jsn0f72b55d1a1b",
+		"X-RapidAPI-Host": "sportspage-feeds.p.rapidapi.com"
+	}
+};
+
+$.ajax(settingsGames).done(function (response) {
+	console.log(response);
+});
+
+
